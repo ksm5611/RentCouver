efa2c05026323e2d5e4c6d66fc77640a0c5e2a70
Sura Jeon <surajeon@gmail.com>
2021-07-03 11:03:08 -0700

    test

--- react-front-end/src/components/Navigation.js
+++ react-front-end/src/components/Navigation.js
@@ -3,7 +3,7 @@ import '../Navigation.css';
 
 export default function Navigation() {
   return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+    <Navbar collapseOnSelect className="nav-color" expand="lg" bg="dark" variant="dark">
       <Navbar.Brand href="#home">RentCouver</Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
