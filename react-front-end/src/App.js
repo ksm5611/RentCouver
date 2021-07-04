401961a58e925688669ebd0e18aee686e4d7941b
Sura Jeon <surajeon@gmail.com>
2021-07-03 19:40:04 -0700

    Nav bar edited

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import axios from 'axios';
 import Navigation from './components/Navigation';
 import User from './components/User/index';
--- react-front-end/src/components/Navigation.js
+++ react-front-end/src/components/Navigation.js
@@ -6,9 +6,9 @@ import 'rsuite/dist/styles/rsuite-default.css';
 export default function Navigation() {
 
   const instance = (
-      <Navbar className="nav-wrapper">
+      <Navbar id="nav-wrapper">
         <Navbar.Header id="nav-logo">
-          <a href="/" className="navbar-brand logo">RentCouver</a>
+          <a href="/" className="navbar-brand logo"><h4>RentCouver</h4></a>
         </Navbar.Header>
         <Navbar.Body>
           <Nav>
@@ -17,10 +17,13 @@ export default function Navigation() {
             <Nav.Item>About Us</Nav.Item>
           </Nav>
           <Nav pullRight>
-            <Dropdown title="Profile" icon={<Icon icon="avatar" />}>
-              <Dropdown.Item>Company</Dropdown.Item>
-              <Dropdown.Item>Team</Dropdown.Item>
-              <Dropdown.Item>Contact</Dropdown.Item>
+            <Dropdown className="test" title="Profile" icon={<Icon icon="avatar" />}>
+              <Dropdown.Item>Account</Dropdown.Item>
+              <Dropdown.Item>My properties</Dropdown.Item>
+              <Dropdown.Item>Received Apps</Dropdown.Item>
+              <Dropdown.Item>My References</Dropdown.Item>
+              <Dropdown.Item>Rent History</Dropdown.Item>
+              <Dropdown.Item>Reference requests</Dropdown.Item>
             </Dropdown>
           </Nav>
         </Navbar.Body>
