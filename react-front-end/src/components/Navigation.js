cc6455ac422eb50d8b0149e4de10f2ea144e605a
Sura Jeon <surajeon@gmail.com>
2021-07-04 03:24:39 -0700

    Nav bg color issue fixed

--- react-front-end/src/components/Navigation.js
+++ react-front-end/src/components/Navigation.js
@@ -1,42 +1,72 @@
 // import { Navbar, Nav, NavDropdown } from 'react-bootstrap'
 import '../App.css'
-import { Navbar, Nav, Icon, Dropdown } from 'rsuite';
+import { Navbar, Icon, Dropdown, Nav } from 'rsuite';
+import NavItem from 'react-bootstrap/Nav';
+import { NavDropdown } from 'react-bootstrap';
 import 'rsuite/dist/styles/rsuite-default.css';
 
 export default function Navigation() {
 
   const instance = (
-      <Navbar id="nav-wrapper">
-        <Navbar.Header id="nav-logo">
-          <a href="/" className="navbar-brand logo"><div><h4>RentCouver</h4></div></a>
-        </Navbar.Header>
-        <Navbar.Body>
-          <Nav>
-            <Nav.Item icon={<Icon icon="home" />} >Home</Nav.Item>
+    <Navbar id="nav-wrapper">
+      <Navbar.Header id="nav-logo">
+        <a href="/" className="navbar-brand logo"><div><h4>RentCouver</h4></div></a>
+      </Navbar.Header>
+      <Navbar.Body className="nav-body">
+        {/* <Nav>
+            {/* <Nav.Item icon={<Icon icon="home" />} >Home</Nav.Item>
             <Nav.Item>Properties</Nav.Item>
-            <Nav.Item>About Us</Nav.Item>
-          </Nav>
-          <Nav className="test" pullRight>
-            <Dropdown className="dropdown-space" title="Profile" icon={<Icon icon="avatar" />}>
-              <Dropdown.Item>Account</Dropdown.Item>
-              <Dropdown.Item>My properties</Dropdown.Item>
-              <Dropdown.Item>Received Apps</Dropdown.Item>
-              <Dropdown.Item>My References</Dropdown.Item>
-              <Dropdown.Item>Rent History</Dropdown.Item>
-              <Dropdown.Item>Reference requests</Dropdown.Item>
-            </Dropdown>
-          </Nav>
-        </Navbar.Body>
-      </Navbar>
+            <Nav.Item>About Us</Nav.Item> </Nav> */}
+        <NavItem
+          activeKey="/home"
+          onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
+          className="nav-body-item-list"
+        >
+          <NavItem.Item  icon={<Icon icon="home" />}>
+            <NavItem.Link className="nav-link" href="/home">Home</NavItem.Link>
+          </NavItem.Item>
+          <NavItem.Item>
+            <NavItem.Link className="nav-link" eventKey="link-1">Properties</NavItem.Link>
+          </NavItem.Item>
+          <NavItem.Item>
+            <NavItem.Link className="nav-link" eventKey="link-2">About Us</NavItem.Link>
+          </NavItem.Item>
+        
+        </NavItem>
+        <NavItem className="nav-body-item-list">
+          <NavDropdown className="profile-tab" title="Profile" id="basic-nav-dropdown">
+            <NavDropdown.Item className="dropdown-item" href="#action/3.1">Dashboard</NavDropdown.Item>
+            <NavDropdown.Item className="dropdown-item" href="#action/3.2">Account</NavDropdown.Item>
+            <NavDropdown.Item className="dropdown-item" href="#action/3.3">My properties</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item className="dropdown-item" href="#action/3.4">Received Apps</NavDropdown.Item>
+            <NavDropdown.Item className="dropdown-item" href="#action/3.4">My References</NavDropdown.Item>
+            <NavDropdown.Item className="dropdown-item" href="#action/3.4">Rent History</NavDropdown.Item>
+            <NavDropdown.Item className="dropdown-item" href="#action/3.4">Reference requests</NavDropdown.Item>
+          </NavDropdown>
+        </NavItem>
+
+        {/* <Nav className="profile-tab" pullRight>
+          <Dropdown className="dropdown-space" title="Profile" icon={<Icon icon="avatar" />}>
+            <Dropdown.Item>Dashboard</Dropdown.Item>
+            <Dropdown.Item>Account</Dropdown.Item>
+            <Dropdown.Item>My properties</Dropdown.Item>
+            <Dropdown.Item>Received Apps</Dropdown.Item>
+            <Dropdown.Item>My References</Dropdown.Item>
+            <Dropdown.Item>Rent History</Dropdown.Item>
+            <Dropdown.Item>Reference requests</Dropdown.Item>
+          </Dropdown>
+        </Nav> */}
+
+
+      </Navbar.Body>
+    </Navbar>
   );
 
   return (
-    <>
+    <div>
       {instance}
-    </>
+    </div>
 
   )
 };
-
-
-{/* <div><h4><span>R</span>ent<span>C</span>ouver</h4></div> */}
--- react-front-end/src/components/User/UserInfo.js
+++ react-front-end/src/components/User/UserInfo.js
@@ -2,12 +2,18 @@
 import { List } from 'rsuite';
 
 export default function UserInfo() {
-  const userList = ['Date of birth: 11/11/2000','Current Address: 2405 Heather St. Vancouver BC','email address: example@gmail.com']
+  const userList = [
+    'Date of birth: 11/11/2000',
+    'Current Address: 2405 Heather St. Vancouver BC',
+    'email address: example@gmail.com',
+    ''
+  ]
   return (
     <section className="user-info-wrapper">
       <div>
         <div>
           <List bordered>
+            <List.Item id= "my-info-label">About</List.Item>
             {userList.map((item, index) => (
               <List.Item key={index} index={index}>
                 {item}
