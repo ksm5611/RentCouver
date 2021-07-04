d50a2794a88d55fee12057e4f6c162cbdb85c323
sumin <kim.sumin.wat2014@gmail.com>
2021-07-04 16:38:33 -0700

    creadted applicationForm api

--- express-back-end/src/routes/applicationForm.js
+++ express-back-end/src/routes/applicationForm.js
@@ -1,8 +1,26 @@
 import { Router } from "express";
 const router = Router();
+import { User, RentHistory, Application } from "../db/models";
 
-router.get("/applications/:tenantId", (req, res) => {
-  res.send("hello");
+//req.param
+router.get("/applications/:tenantId", async (req, res) => {
+  const applicationForm = await User.findOne({
+    where: { id: req.params.tenantId },
+    include: [
+      {
+        model: Application,
+        where: { tenant_id: req.params.tenantId },
+        require: false,
+        attributes: { exclude: ["is_landlord", "password"] },
+      },
+      {
+        model: RentHistory,
+        where: { tenant_id: req.params.tenantId },
+        require: false,
+      },
+    ],
+  });
+  res.send(applicationForm);
 });
 
 export default router;
