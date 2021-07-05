84d458e153b6da906f383a71d29858999c7d9689
Sura Jeon <surajeon@gmail.com>
2021-07-05 02:42:14 -0700

    sidebar file deleted, user.scss deleted. Property Detail page buttons edited

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -21,8 +21,8 @@ export default function App() {
       {/* <Map /> */}
       {/* <User /> */}
       {/* <Homepage /> */}
-      {/* <PropertyDetails /> */}
-      <PropertyListing />
+      <PropertyDetails />
+      {/* <PropertyListing /> */}
     </>
   );
 }
