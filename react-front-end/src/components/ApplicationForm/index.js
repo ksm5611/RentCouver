276289808bfadaaad132bc47d5c13412a8af75ca
sumin <kim.sumin.wat2014@gmail.com>
2021-07-04 19:55:53 -0700

    design application form and fetch the data from db

--- react-front-end/src/components/ApplicationForm/index.js
+++ react-front-end/src/components/ApplicationForm/index.js
@@ -1,11 +1,33 @@
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import {
+  ListItem,
+  ListItemText,
+  List,
+  Container,
+  Typography,
+  makeStyles,
+} from "@material-ui/core";
+
+const useStyles = makeStyles(() => {
+  return {
+    root: {
+      border: "1px solid black",
+      marginBottom: "16px",
+    },
+  };
+});
 
 export default function ApplicationForm() {
-  const application = [
+  const classes = useStyles();
+  const [user, setUser] = useState(null);
+  const [rentHistories, setRentHistories] = useState([]);
+
+  const userInfo = [
     { label: "Name", value: "name" },
-    { label: "Name", value: "name" },
-    { label: "Name", value: "name" },
+    { label: "Job title", value: "job_title" },
+    { label: "Current address", value: "current_address" },
+    { label: "Email address", value: "email" },
   ];
 
   useEffect(() => {
@@ -14,23 +36,67 @@ export default function ApplicationForm() {
         const result = await axios.get(
           "http://localhost:8000/api/applications/10"
         );
-        console.log(result.data);
+        const { RentHistories: rentHistoryData, ...userData } = result.data;
+        setUser(userData);
+        setRentHistories(rentHistoryData);
+        console.log(rentHistoryData);
       } catch (error) {
         console.log(error);
       }
     }
     fetchData();
   }, []);
+
+  const renderUserData = () => {
+    return (
+      <List>
+        {userInfo.map((info) => (
+          <ListItem key={info.value} className={classes.root}>
+            <ListItemText primary={info.label} secondary={user[info.value]} />
+          </ListItem>
+        ))}
+      </List>
+    );
+  };
+
+  const renderRentHistory = (rentHistory) => {
+    return (
+      <List className={classes.root}>
+        <ListItem>
+          <ListItemText
+            primary="Address"
+            secondary={formatAddressFromProperty(rentHistory.Property)}
+          />
+        </ListItem>
+        <ListItem>
+          <ListItemText
+            primary="Period"
+            secondary={formatRentHistoryPeriod(rentHistory)}
+          />
+        </ListItem>
+      </List>
+    );
+  };
+
+  const formatRentHistoryPeriod = (rentHistory) => {
+    return `${rentHistory.start_date} - ${rentHistory.end_date}`;
+  };
+
+  const formatAddressFromProperty = (property) => {
+    return `${property.street}, ${property.city}, ${property.province} ${property.postal_code}`;
+  };
+
   return (
-    <div>
-      {application.map((label) => {
-        return (
-          <div key={label.value}>
-            <label>{label.label}</label>
-            <p>{label.value}</p>
-          </div>
-        );
-      })}
-    </div>
+    <Container maxWidth="md">
+      <Typography variant="h4">About yourself</Typography>
+
+      {user && renderUserData()}
+      {rentHistories.length > 0 && (
+        <>
+          <Typography>Residential history</Typography>
+          {rentHistories.map((history) => renderRentHistory(history))}
+        </>
+      )}
+    </Container>
   );
 }
