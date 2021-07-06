3177275b689e9d4e6afbebcafb8b8a1bd5468a21
Sura Jeon <surajeon@gmail.com>
2021-07-06 16:00:49 -0700

    Rent history page displays history data

--- react-front-end/src/components/RentHistory/index.js
+++ react-front-end/src/components/RentHistory/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, Fragment } from 'react';
 import {
   Container,
   ListItem,
@@ -15,7 +15,7 @@ const useStyles = makeStyles((theme) => {
       border: "1px solid black",
       marginBottom: "16px",
       display: "flex",
-      justifyContent: "space-between"      
+      justifyContent: "space-between"
     },
     btn: {
       backgroundColor: '#f1a177',
@@ -27,44 +27,47 @@ const useStyles = makeStyles((theme) => {
   };
 });
 
-export default function RentHistory () {
+export default function RentHistory() {
 
   const classes = useStyles();
 
-  const [user, setUser] = useState(null);
-  const [properties, setProperty] = useState();
-  const [rentHistories, setRentHistories] = useState([]);
-  const [error, setError] = useState("");
-
-  const propertyInfo = [
-    {value: "street"},
-    {value: "unit"},
-    {value: "city"},
-    {value: "province"},
-    {value: "postal_code"}    
-  ]
+  const [history, setHistory] = useState([]);
+  const [error, setError] = useState(false);
+  const [pending, setPending] = useState(true);
 
   useEffect(() => {
-    async function fetchData() {
-      try {
-        const result = await axios.get(
-          "http://localhost:8000/api/rentHistories"
-        );
-        const { Property: propertyData, ...rentHistoryData} = result.data;
-
-        setProperty(propertyData);
-        setRentHistories(rentHistoryData);
-        // setUser(userData);
-      } catch (error) {
-        setError("Your server is broken");
-      }
-    }
-    fetchData();
+    axios.get("http://localhost:8000/api/rentHistories")
+      .then((result) => {
+        setHistory(result.data);
+        setError(false)
+      })
+      .catch((err) => {
+        console.log(err.message);
+        setError(true)
+      })
+      .finally(() => {
+        setPending(false);
+      })
   }, []);
 
-  if (!rentHistories) {
-    return <div>Loading..</div>;
-  }
+
+  const historyList = history.map((record) => {
+    return (
+
+      <ListItem className={classes.root}>
+        {/* <Address /> */}
+        <div className="history-info">
+          <p className="req-landlord address">{record.Property.street}, {record.Property.unit && <>#{record.Property.unit}</>} {record.Property.city} {record.Property.province} {record.Property.postal_code}</p>
+          <p className="req-landlord">{record.start_date} - {record.end_date}</p>
+          <span>{record.Property.User.name}</span>
+        </div>
+        <div className="option-btn">
+          <Button className={classes.btn} variant="contained">Request Reference</Button>
+        </div>
+      </ListItem>
+
+    )
+  })
 
 
   return (
@@ -75,20 +78,17 @@ export default function RentHistory () {
         </div>
       </section>
       <Container>
-
-        <List>
-
-          <ListItem className={classes.root}>
-            <div className="history-info">
-              <p className="req-tenant address">{rentHistories.start_date}</p>
-              <p className="req-tenant">Start date - End date</p>
-              <span>Landlord Name</span>
-            </div>
-            <div className="option-btn">
-              <Button className={classes.btn} variant="contained">Request for reference from prev landlord</Button>
+        {error && <div>Error Loading data</div>}
+        {pending && <div>Please wait</div>}
+        <ListItem className={classes.root} id="listitem-head">
+            <div className="rent-history-info">
+              <h5 className="rent-historty-address">Property Address</h5>
+              <h5>Rent Period</h5>
+              <h5>Owner Name</h5>
             </div>
           </ListItem>
-
+        <List>
+          {historyList}
         </List>
       </Container>
 
