9dd22dc14800cf78912d3d39e6f4d28f5ad6efc8
sumin <kim.sumin.wat2014@gmail.com>
2021-07-05 19:25:43 -0700

    removed the unused function

--- react-front-end/src/components/PropertyDetails/Slideshow.js
+++ react-front-end/src/components/PropertyDetails/Slideshow.js
@@ -4,14 +4,7 @@ import { useEffect, useState, setError } from "react";
 import { useParams } from "react-router-dom";
 import { Slide } from "react-slideshow-image";
 
-const slideImages = [
-  "https://images.unsplash.com/photo-1580587771525-78b9dba3b914?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1267&q=80",
-  "https://images.unsplash.com/photo-1554995207-c18c203602cb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
-  "https://images.unsplash.com/photo-1484154218962-a197022b5858?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1353&q=80",
-];
-
 const Slideshow = () => {
-  // const [user, setUser] = useState(null);
   const [property, setProperty] = useState(null);
   const [photos, setPhotos] = useState(null);
 
@@ -23,7 +16,6 @@ const Slideshow = () => {
           `http://localhost:8000/api/properties/${id}`
         );
         const { Photos: photoData, ...propertyData } = result.data;
-        // setUser(userData);
         setProperty(propertyData);
         setPhotos(photoData);
       } catch (error) {
@@ -53,4 +45,3 @@ const Slideshow = () => {
 };
 
 export default Slideshow;
-// style={{ backgroundImage: `url(${photo.photo_url})` }}
