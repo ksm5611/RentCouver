c64d65cf7f937129bb3c5e3fd46a16f23f73706b
Sura Jeon <surajeon@gmail.com>
2021-07-07 15:34:57 -0700

    application list: api post request added for decline

--- react-front-end/src/components/ApplicationList/index.js
+++ react-front-end/src/components/ApplicationList/index.js
@@ -39,6 +39,7 @@ const useStyles = makeStyles((theme) => {
 export default function ApplicationList() {
   const [appLists, setAppLists] = useState([]);
   const [error, setError] = useState("");
+  const [appDecision, setAppDecision] = useState(false);
 
   useEffect(() => {
     async function fetchData() {
@@ -51,6 +52,23 @@ export default function ApplicationList() {
     }
     fetchData();
   }, []);
+
+  useEffect(()=>{
+    async function updateStatus() {
+      try {
+
+      } catch {
+        setError("NOPE")
+      }
+    }
+    updateStatus();
+  },[])
+
+  const declineApp = async (appID) => {
+    await axios.post(`http://localhost:8000/api/appList/${appID}`);    
+
+  }
+
 
   //material ui styling funtion
   const classes = useStyles();
@@ -87,13 +105,18 @@ export default function ApplicationList() {
                 </div>
                 <div className="option-btn">
                   <ApplicationReview tenantId={listValue.tenant_id} />
+                  {listValue.is_decline === false ? (
                   <Button
                     className={classes.btn}
                     variant="contained"
                     color="secondary"
+                    onClick={() => {declineApp(listValue.id)}}
                   >
                     Decline
                   </Button>
+                  ) :(
+                    <p>Declined</p>
+                  )}
                 </div>
               </ListItem>
             );
