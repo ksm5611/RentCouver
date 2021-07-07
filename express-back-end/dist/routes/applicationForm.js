5b62f66ccfb460b73399c7f3f588cadd3c31f096
sumin <kim.sumin.wat2014@gmail.com>
2021-07-06 22:34:38 -0700

    cleaning

---
+++ express-back-end/dist/routes/applicationForm.js
@@ -0,0 +1,95 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var _express = require("express");
+
+var _models = require("../db/models");
+
+function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
+
+function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
+
+var router = (0, _express.Router)();
+//req.param will find
+router.get("/applications/:tenantId", /*#__PURE__*/function () {
+  var _ref = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee(req, res) {
+    var applicationForm;
+    return regeneratorRuntime.wrap(function _callee$(_context) {
+      while (1) {
+        switch (_context.prev = _context.next) {
+          case 0:
+            _context.next = 2;
+            return _models.User.findOne({
+              where: {
+                id: req.params.tenantId
+              },
+              attributes: {
+                exclude: ["is_landlord", "password"]
+              },
+              include: [{
+                model: _models.RentHistory,
+                where: {
+                  tenant_id: req.params.tenantId
+                },
+                require: false,
+                attributes: {
+                  exclude: ["is_requested", "is_decline", "review_content"]
+                },
+                include: [{
+                  model: _models.Property,
+                  attributes: {
+                    exclude: ["title", "square_feet", "description", "property_type", "number_of_bathrooms", "number_of_bedrooms", "listed_start_date", "cost_of_month", "pets_allowed"]
+                  }
+                }]
+              }]
+            });
+
+          case 2:
+            applicationForm = _context.sent;
+            res.send(applicationForm);
+
+          case 4:
+          case "end":
+            return _context.stop();
+        }
+      }
+    }, _callee);
+  }));
+
+  return function (_x, _x2) {
+    return _ref.apply(this, arguments);
+  };
+}()); //submit application form call
+
+router.post("/applications", /*#__PURE__*/function () {
+  var _ref2 = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee2(req, res) {
+    var applicationForm;
+    return regeneratorRuntime.wrap(function _callee2$(_context2) {
+      while (1) {
+        switch (_context2.prev = _context2.next) {
+          case 0:
+            _context2.next = 2;
+            return _models.Application.create(req.body);
+
+          case 2:
+            applicationForm = _context2.sent;
+            res.json(applicationForm);
+
+          case 4:
+          case "end":
+            return _context2.stop();
+        }
+      }
+    }, _callee2);
+  }));
+
+  return function (_x3, _x4) {
+    return _ref2.apply(this, arguments);
+  };
+}());
+var _default = router;
+exports["default"] = _default;
