9d316734261dcf27d3f92d0672d89bb7be1d5e62
sumin <kim.sumin.wat2014@gmail.com>
2021-07-09 15:06:25 -0700

    untracked file

---
+++ express-back-end/dist/db/models/property.js
@@ -0,0 +1,143 @@
+"use strict";
+
+function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+function _createSuper(Derived) { var hasNativeReflectConstruct = _isNativeReflectConstruct(); return function _createSuperInternal() { var Super = _getPrototypeOf(Derived), result; if (hasNativeReflectConstruct) { var NewTarget = _getPrototypeOf(this).constructor; result = Reflect.construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return _possibleConstructorReturn(this, result); }; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Boolean.prototype.valueOf.call(Reflect.construct(Boolean, [], function () {})); return true; } catch (e) { return false; } }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+var _require = require("sequelize"),
+    Model = _require.Model;
+
+module.exports = function (sequelize, DataTypes) {
+  var Property = /*#__PURE__*/function (_Model) {
+    _inherits(Property, _Model);
+
+    var _super = _createSuper(Property);
+
+    function Property() {
+      _classCallCheck(this, Property);
+
+      return _super.apply(this, arguments);
+    }
+
+    _createClass(Property, null, [{
+      key: "associate",
+      value:
+      /**
+       * Helper method for defining associations.
+       * This method is not a part of Sequelize lifecycle.
+       * The `models/index` file will call this method automatically.
+       */
+      function associate(models) {
+        // define association here
+        this.belongsTo(models.User, {
+          foreignKey: "landlord_id",
+          onDelete: "CASCADE"
+        });
+        this.hasMany(models.Photo, {
+          foreignKey: "property_id"
+        });
+        this.hasMany(models.RentHistory, {
+          foreignKey: "property_id"
+        });
+        this.hasMany(models.Application, {
+          foreignKey: "property_id"
+        });
+        this.hasMany(models.Ref_request, {
+          foreignKey: "property_id"
+        });
+      }
+    }]);
+
+    return Property;
+  }(Model);
+
+  Property.init({
+    landlord_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    street: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    unit: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    city: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    province: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    postal_code: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    square_feet: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 0
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: true
+    },
+    property_type: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    number_of_bathrooms: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 0
+    },
+    number_of_bedrooms: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 0
+    },
+    listed_start_date: {
+      type: DataTypes.DATE,
+      allowNull: false
+    },
+    cost_of_month: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 0
+    },
+    pets_allowed: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false
+    }
+  }, {
+    sequelize: sequelize,
+    tableName: "properties",
+    timestamps: false
+  });
+  return Property;
+};
---
+++ express-back-end/dist/routes/applicationForm.js
@@ -0,0 +1,125 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var _express = require("express");
+
+var _models = require("../db/models");
+
+function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
+
+function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
+
+var router = (0, _express.Router)();
+//req.param will find
+router.get("/applications/:tenantId", /*#__PURE__*/function () {
+  var _ref = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee(req, res) {
+    var applicationForm;
+    return regeneratorRuntime.wrap(function _callee$(_context) {
+      while (1) {
+        switch (_context.prev = _context.next) {
+          case 0:
+            _context.next = 2;
+            return _models.User.findOne({
+              where: {
+                id: req.params.tenantId
+              },
+              attributes: {
+                exclude: ["password"]
+              },
+              include: [{
+                model: _models.RentHistory,
+                where: {
+                  tenant_id: req.params.tenantId
+                },
+                require: false,
+                attributes: {
+                  exclude: ["is_requested", "is_decline", "review_content"]
+                },
+                include: [{
+                  model: _models.Property,
+                  attributes: {
+                    exclude: ["title", "square_feet", "description", "property_type", "number_of_bathrooms", "number_of_bedrooms", "listed_start_date", "cost_of_month", "pets_allowed"]
+                  }
+                }]
+              }]
+            });
+
+          case 2:
+            applicationForm = _context.sent;
+            res.send(applicationForm);
+
+          case 4:
+          case "end":
+            return _context.stop();
+        }
+      }
+    }, _callee);
+  }));
+
+  return function (_x, _x2) {
+    return _ref.apply(this, arguments);
+  };
+}()); //submit application form call
+
+router.post("/applications", /*#__PURE__*/function () {
+  var _ref2 = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee2(req, res) {
+    var rentHistory, applicationForm;
+    return regeneratorRuntime.wrap(function _callee2$(_context2) {
+      while (1) {
+        switch (_context2.prev = _context2.next) {
+          case 0:
+            if (!req.body.renthistories_id) {
+              _context2.next = 14;
+              break;
+            }
+
+            _context2.prev = 1;
+            _context2.next = 4;
+            return _models.RentHistory.findOne({
+              where: {
+                id: req.body.renthistories_id
+              }
+            });
+
+          case 4:
+            rentHistory = _context2.sent;
+            rentHistory.is_requested = true;
+            _context2.next = 8;
+            return rentHistory.save();
+
+          case 8:
+            res.send(rentHistory);
+            _context2.next = 14;
+            break;
+
+          case 11:
+            _context2.prev = 11;
+            _context2.t0 = _context2["catch"](1);
+            res.send(_context2.t0);
+
+          case 14:
+            _context2.next = 16;
+            return _models.Application.create(req.body);
+
+          case 16:
+            applicationForm = _context2.sent;
+            res.send(applicationForm);
+
+          case 18:
+          case "end":
+            return _context2.stop();
+        }
+      }
+    }, _callee2, null, [[1, 11]]);
+  }));
+
+  return function (_x3, _x4) {
+    return _ref2.apply(this, arguments);
+  };
+}());
+var _default = router;
+exports["default"] = _default;
