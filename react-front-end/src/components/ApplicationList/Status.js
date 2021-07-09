de640369bcd164ce38fdd360f08a7c5ff2baa1c6
Sura Jeon <surajeon@gmail.com>
2021-07-08 20:31:23 -0700

    applicationList button fixed

--- react-front-end/src/components/ApplicationList/Status.js
+++ react-front-end/src/components/ApplicationList/Status.js
@@ -3,18 +3,11 @@ import useVisualMode from '../RefReqList/RefReqItem/useVisualMode';
 import ApplicationReview from "../ApplicationReview";
 
 import {
-  Container,
-  ListItem,
-  ListItemText,
-  List,
-  Typography,
   Button,
-  TextField,
-  makeStyles,
   Avatar,
 } from "@material-ui/core";
 
-export default function Status ({listValue}) {
+export default function Status({ listValue }) {
   const DEFAULT = "DEFAULT";
   const DECLINED = "DECLINED";
   const { mode, transition } = useVisualMode(DEFAULT);
@@ -27,35 +20,37 @@ export default function Status ({listValue}) {
 
   return (
     <>
-    <div className="req-info">
-                  <Avatar src={listValue.tenant.profile_picture_url} />
-                  <p className="req-tenant">{listValue.tenant.name}</p>
-                  <p className="req-tenant address">
-                    {listValue.Property.street}
-                    {listValue.Property.unit}
-                    {listValue.Property.city}
-                    {listValue.Property.provice}
-                    {listValue.Property.postal_code}
-                  </p>
-                </div>
-                <div className="option-btn">
-                  <ApplicationReview tenantId={listValue.tenant_id} />
-                  {mode === DEFAULT && (
-                    listValue.is_decline === false ? (
-                      <Button
-                        // className={classes.btn}
-                        variant="contained"
-                        color="secondary"
-                        onClick={() => { declineApp(listValue.id) }}
-                      >
-                        Decline
-                      </Button>
-                    ) : (
-                      DECLINED
-                    )
-                  )}
-                  {mode === DECLINED && (<p>DECLINED</p>)}
-                </div>
+      <div className="req-info">
+        <Avatar src={listValue.tenant.profile_picture_url} />
+        <p className="req-tenant">{listValue.tenant.name}</p>
+        <p className="req-tenant address">
+          {listValue.Property.street}
+          {listValue.Property.unit}
+          {listValue.Property.city}
+          {listValue.Property.provice}
+          {listValue.Property.postal_code}
+        </p>
+      </div>
+      <div className="option-btn">
+        {mode === DEFAULT && (
+          listValue.is_decline === false ? (
+            <>
+              <ApplicationReview tenantId={listValue.tenant_id} />
+              <Button
+                // className={classes.btn}
+                variant="contained"
+                color="secondary"
+                onClick={() => { declineApp(listValue.id) }}
+              >
+                Decline
+                </Button>
+            </>
+          ) : (
+            DECLINED
+          )
+        )}
+        {mode === DECLINED && (<p>DECLINED</p>)}
+      </div>
     </>
   )
 }
